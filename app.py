9f94a0d2f4eb203763f714cdcd8e4fc19ec5e7de
SHIVAN KUMAR <51358670+Shivan118@users.noreply.github.com>
2026-05-11 17:42:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-support_number = "62809641256"
+support_number = "+91 62809641256"
 @app.route('/'):
 def index():
     return "Hello shivan18july1997@gmail.com
